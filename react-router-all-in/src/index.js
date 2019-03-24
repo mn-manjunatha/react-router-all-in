a0465fa0e769f88c8060739ee36330f5031eda98
unknown <manjunath2.mn@gmail.com>
2019-03-24 21:30:17 +0530

    changes

--- react-router-all-in/src/index.js
+++ react-router-all-in/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 
+
 import App from "./App";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
